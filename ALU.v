fcb85e44735f36e41197c878e94142746b2aaa1e
Pecosaurus <99046305+Pecosaurus@users.noreply.github.com>
2022-04-28 20:52:00 -0400

    Update ALU.v
    Updated ALU file. This ALU code works

--- ALU.v
+++ ALU.v
@@ -1,11 +1,14 @@
 module ALU (A, B, C_in, Select, ALU_out, Status);
 
+
+
 input [31:0] A;
 input [31:0] B;
-input [2:0] Select; 
+//input MuxSel;
+input [3:0] Select; 
 input C_in;
 
-output  [31:0] ALU_out;
+output [31:0] ALU_out;
 output reg [3:0] Status;
 
 wire C_out;
@@ -13,10 +16,21 @@ reg V, Z, N;
 wire [31:0] I4, I5, I6;
 reg[31:0] I0, I1, I2, I3, I7;
 
-adder32bit dut1 (A,B, C_in, C_out, I4);
+
+adder_subtractor32bit dut1 (A,B,C_in, Select[3], C_out,I4);
+
 shift_register dut2 (A, B, I5, I6);
+ 
+always @ (ALU_out) begin
+	
+	N = A[31] ^ B[31]; //Negative
+	//assign C_out = ALU_out[31]; //carryout
+	V = ((A[31] & B[31]) | (~A[31] & ~B[31])); //overflow
+	Z = (ALU_out[30:0] == 0) ? 1'b1 : 1'b0; // Zero
 
+	Status = {C_out, N, V, Z};
 
+end
 
 always @( A or B)
 
@@ -34,29 +48,7 @@ begin
 
 end	
 	
-	Mux_8to1 dut3 (ALU_out,I0,I1,I2,I3,I4,I5,I6,I7,Select);
+	Mux_8to1 dut3 (ALU_out,I0,I1,I2,I3,I4,I5,I6,I7,Select[2:0]);
 	
-always @ (ALU_out or I4 or C_out)
-	begin
-	
-	if (Select == 3'b100)
-		Status[0] = C_out; // Status[0] = C_out: Carryout
-	else Status[0] = 0;
-	
-	if (Select == 3'b100)
-		Status[1] = ((~A[31]) & (~B[31]) & I4[31]) | ( A[31] & B[31] & (~I4[31]));    // Status[1] = V: Overflow
-	else Status[1] = 0;
-	
-	
-	if (ALU_out == 32'b0)
-			Status[2] = 1;              //Status[2] is Z
-		else Status[2] = 0; 
-
-	
-		
-	Status [3] = ALU_out[31];  // Status[3] = N: Negative
-	
-end
-
 
 endmodule
